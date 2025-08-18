35553c0f1fb1fa64e6c6da68f13b27345456a308
Vishwesh <vishweshpatial@gmail.com>
2025-08-18 16:51:25 +0530

    Data ingestion Pipeline Executed Successfully

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -2,10 +2,11 @@ import os
 from box.exceptions import BoxValueError
 import yaml
 from textSummarizer.logging import logger
-from ensure import ensure_annotations
+# from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+from typeguard import typechecked as ensure_annotations
 
 
 
